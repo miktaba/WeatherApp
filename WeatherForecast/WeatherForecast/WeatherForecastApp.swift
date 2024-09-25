68f6c0392651809d1d9da222a6e6491da8a0e225
miktaba <mtabakaev@gmail.com>
2024-09-25 02:56:32 +0200

    Work with user interface and design
    - added MeshGradient to the background and its animation depending on the time of day.
    - added switching between MeshGradient and normal gradient depending on iOS version.

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Mikhail Tabakaev on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
